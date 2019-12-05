51a9ddef3bfaf15b1c65fd07ccef1d94a21e2776
Siddhant Sinha <sid.sinha94@gmail.com>
2019-12-05 17:40:20 +0530

    added question pdf file, intermediate commit for main func logic and utility func

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"geektrust/utility"
 	"fmt"
+	"geektrust/utility"
 )
 
 const inputFile = "input1.txt"
@@ -10,6 +10,10 @@ const inputFile = "input1.txt"
 var messages []string
 
 func main() {
+	// TODO: Make input file to be read from os.Args
 	messages := utility.ReadInput(inputFile)
 	fmt.Println(messages)
+	resultMap := utility.DecryptMessages(messages)
+	fmt.Println(resultMap)
+
 }
--- utility/utility.go
+++ utility/utility.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 func ReadInput(filename string) []string {
@@ -16,7 +17,7 @@ func ReadInput(filename string) []string {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines) // Tokenizes based on new line
 	for scanner.Scan() {           // Value is false when EOF or error
-		fmt.Println(scanner.Text())
+		//fmt.Println(scanner.Text())
 		result = append(result, scanner.Text())
 	}
 
@@ -28,4 +29,27 @@ func ReadInput(filename string) []string {
 		return []string{}
 	}
 	return result
+}
+
+// ProcessMessages takes a slice of encrypted messages and decrypts it
+func DecryptMessages(messages []string) map[string]string {
+	resultMap := make(map[string]string)
+	// const msgRegex = `(?i)(?P<Name>Air|Land|Water|Ice|Fire),(?:\s*)"(?P<Message>.*?)"`
+	const msgRegex = `(?i)(?P<Name>Air|Land|Water|Ice|Fire)(?:\s)(?P<Message>.*)`
+	r, err := regexp.Compile(msgRegex)
+	if err != nil {
+		fmt.Printf("Failed to compile Regex.", err)
+		return resultMap
+	}
+
+	for _, val := range messages {
+		//matches := r.FindAllString(val, -1)
+		match := r.FindStringSubmatch(val)
+		for i, name := range r.SubexpNames() {
+			if i > 0 && i <= len(match) {
+				resultMap[name] = match[i]
+			}
+		}
+	}
+	return resultMap
 }
